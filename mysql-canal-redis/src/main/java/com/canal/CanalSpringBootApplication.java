0644dce48094f7734a37b8e9aef102f16541719b
wazhdeveloper <93420381+wazhdeveloper@users.noreply.github.com>
2023-02-24 19:50:27 +0800

    Add files via upload
    redis-canal-mysql架构，使用canal保证redis和mysql读写一致，微服务组件

---
+++ mysql-canal-redis/src/main/java/com/canal/CanalSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wazh
+ * @description
+ * @since ${YEAR}-${MONTH}-${DAY}-${TIME}
+ */
+@SpringBootApplication
+public class CanalSpringBootApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(CanalSpringBootApplication.class,args);
+    }
+}
---
+++ mysql-canal-redis/src/main/java/com/canal/utils/RedisUtil.java
@@ -0,0 +1,27 @@
+package com.canal.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisUtil {
+    public static final String  REDIS_IP_ADDR = "192.168.88.100";
+    public static final String  REDIS_pwd = "";
+    public static JedisPool jedisPool;
+
+    static {
+        JedisPoolConfig jedisPoolConfig=new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(20);
+        jedisPoolConfig.setMaxIdle(10);
+        jedisPool=new JedisPool(jedisPoolConfig,REDIS_IP_ADDR,6379,10000,REDIS_pwd);
+    }
+
+    public static Jedis getJedis() throws Exception {
+        if(null!=jedisPool){
+            return jedisPool.getResource();
+        }
+        throw new Exception("Jedispool is not ok");
+    }
+
+
+}
